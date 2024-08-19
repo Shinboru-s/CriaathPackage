f1b8bbd12e4269e0add5f58e12a01b2f089faeea
EmrecanDedeoglu <emrecandedeoglu@gmail.com>
2024-08-19 13:43:55 +0300

    Update DropdownOptionDrawer.cs

--- Runtime/Attributes/DropdownOptionAttribute/DropdownOptionDrawer.cs
+++ Runtime/Attributes/DropdownOptionAttribute/DropdownOptionDrawer.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
@@ -73,3 +74,4 @@ public class DropdownOptionDrawer : PropertyDrawer
         return EditorGUIUtility.singleLineHeight;
     }
 }
+#endif
